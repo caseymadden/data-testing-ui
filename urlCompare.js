e440274a9ff056c204e997744489d51c35d1c26e
caseymadden <casey.c.madden@gmail.com>
2016-02-07 23:13:42 -0500

    wrong JS file in github

---
+++ urlCompare.js
@@ -0,0 +1,48 @@
+var compareApp = angular.module('compareApp', []);
+
+compareApp.controller('compareScreenCtrl', function ($scope, $http) {
+ $scope.url = "";
+ $scope.getUrl = function(url) {
+  $scope.inputUrl = url;
+ }
+
+//  $scope.submitScraperFeedback = function(approved, issue, note) {
+//   $scope.scraperFeedback = { "approved" : approved,
+//    "issue" : issue,
+//    "note" : note }
+//    console.log($scope.scraperFeedback);
+//    $http({
+//   	method : 'POST',
+//   	url : 'return_compare.json',
+//   	data : $scope.scraperFeedback
+//   })
+//  };
+// });
+
+ $scope.submitScraperFeedback = function(approved, issue, note) {
+  var data = $.param({
+  	"approved" : approved,
+  	"issue" : issue,
+  	"note" : note
+  });
+  var config = {
+  	headers: {
+  		'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8;'
+  	}
+   }
+  $http.post('return_compare.json', data, config)
+  .success(function (data, status, headers, config) {
+  	console.log(data);
+  	$scope.postDataResponse = data;
+  	console.log("success");
+
+  })
+  .error(function(data, status, header, config) {
+  	$scope.responseDetails = "Data: " + data +
+  	"<hr />status: " + status +
+  	"<hr />headers: " + header + 
+  	"<hr />config: " + config;
+  });
+ };
+  
+});
--- url_compare.js
+++
@@ -1,8 +0,0 @@
-var compareApp = angular.module('compareApp', []);
-
-compareApp.controller('compareScreenCtrl', function ($scope) {
-	$scope.url = "";
-  $scope.getUrl = function(url) {
-  	$scope.inputUrl = url;
-  }
-});
